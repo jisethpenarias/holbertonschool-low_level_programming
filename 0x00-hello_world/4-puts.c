974994481fbccf89b787db376c306cd785481866
jpenariasholberton <1875@holbertonschool.com>
2020-06-10 23:56:58 +0000

    4.puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-/* using puts */
-
 #include <stdio.h>
+
+/* printing with puts*/
 
 int main ()
 {
-  puts("Programming is like building a multilingual puzzle"\n);
+  puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
