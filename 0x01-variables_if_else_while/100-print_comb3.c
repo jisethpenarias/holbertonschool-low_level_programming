493cd2b1d55b0f7e617526bed0f81713762a78bb
Jiseth Peña <66282703+jpenariasholberton@users.noreply.github.com>
2020-06-14 21:19:04 -0500

    Print comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - a simple program that outputs 0-9 separated by commas
+ *
+ * Return: 0 on success
+ */
+ 
+int main()
+{
+    int number;
+    int secondNumber;
+    int limit;
+    limit = 57;
+    number = 48;
+    secondNumber = 48;
+    int iterator = 0;
+
+    while(number <= 57) {
+        secondNumber = 48 + iterator;
+        while(secondNumber <= 57) {
+            if(number != secondNumber){
+                putchar(number);
+                putchar(secondNumber);
+                if(number != 56 || secondNumber != 57){
+                    putchar(44);
+                    putchar(32);    
+                }
+            }
+            secondNumber++;
+        }    
+        number++;
+        iterator++;
+    }
+    putchar('\n');
+    return 0;
+}
