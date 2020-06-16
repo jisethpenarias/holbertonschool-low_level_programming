ee837129366e9805f331c7942fa7c4e474e53b67
jpenariasholberton <1875@holbertonschool.com>
2020-06-16 04:56:41 +0000

    header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int print_last_digit(int);
+int _abs(int);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+#endif
