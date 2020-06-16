c428a2758e6949370c16e346a1ac0c3a37549f46
jpenariasholberton <1875@holbertonschool.com>
2020-06-16 19:15:15 +0000

    task aand b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *add - function print add two intergerst
+ *
+ *@a: number interger one
+ *@b: number interger two
+ *Return: two intergerst
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
