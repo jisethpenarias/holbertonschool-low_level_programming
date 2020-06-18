f49c4ec7d50de6a846348e097b9f07892274107b
jpenariasholberton <1875@holbertonschool.com>
2020-06-18 20:50:08 +0000

    more numbers task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,27 @@
 #include "holberton.h"
 
 /**
- *more_numbers- function prints 0-14 fllowed
+ *more_numbers- function prints 0-14 followed
  *
  *Return: void
  */
 
 void more_numbers(void)
 {
-	int n;
-	int i = 0;
+int n;
+int i = 0;
 
-while (i <= 9) /* aqui se reccorre de 1 a 9*/
+while (i <= 9)
 {
-for (n = 0; n <= 14; n++) /*  aqui se recorre de a 1 a 14*/
+for (n = 0; n <= 14; n++)
 {
-if (n >= 9)/* solo en los casos en que n sea mayor a 9*/
+if (n >= 9)
 {
-_putchar(48 + (n / 10)); /* se imprime el primer digito*/
+_putchar(48 + (n / 10));
 }
-_putchar(48 + (n % 10)); /*modulo de 10 de un num de 1 digito es el mismo nro*/
+_putchar(48 + (n % 10));
 }
 _putchar('\n');
-i++; /* se itera para poder hacer los 9 filas*/
+i++;
 }
-return;
 }
