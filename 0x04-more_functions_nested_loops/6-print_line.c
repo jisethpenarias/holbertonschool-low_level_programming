6417af4d6640a70cea28ee69cdf3b3f7d15a6cdc
jpenariasholberton <1875@holbertonschool.com>
2020-06-18 23:49:43 +0000

    print under line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_line  - function print line and receives as input n
+ *
+ *@n: input variable
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
