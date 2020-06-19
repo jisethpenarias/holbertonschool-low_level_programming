5744c68d02bfaf34aacf3bc487617c81bb698374
jpenariasholberton <1875@holbertonschool.com>
2020-06-19 02:59:33 +0000

    print hash

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *print_square  - function print square
+ *
+ *@size: seize input
+ *
+ *Return: void
+ */
+
+
+void print_square(int size)
+{
+int i;
+int hash;
+
+i = 0;
+
+while (i < size)
+{
+hash = 0;
+while (hash < size)
+{
+_putchar(35);
+hash++;
+}
+_putchar ('\n');
+i++;
+}
+}
