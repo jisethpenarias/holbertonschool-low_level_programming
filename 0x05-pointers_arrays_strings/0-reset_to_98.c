a677dbf916942fa5ec509c54a8fc353cbcec236a
jpenariasholberton <1875@holbertonschool.com>
2020-06-22 22:39:25 +0000

    reset to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - take a pointer to an int as parameter and updates the evalue
+ *
+ *@n: pointer to an integer
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
