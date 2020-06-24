7e91045b61a6a44c177e1f64c72e03a6d3b1a467
jpenariasholberton <1875@holbertonschool.com>
2020-06-24 02:53:05 +0000

    last exercise solved

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strcpy - copy char array
+* @dest : dest
+* @src : source
+*
+* Return: desc
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
