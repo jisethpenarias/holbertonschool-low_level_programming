54aee9edb55d4eb1a290349912c1cf5e1e16ec6a
Jiseth Peña <66282703+jpenariasholberton@users.noreply.github.com>
2020-06-27 09:53:39 -0500

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,42 @@
 #include "holberton.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @a: string to capitalize
- * Return: string;
- */
+  * cap_string - Capitalizes all words of a strin
+  *
+  * @s: pointer to the string
+  *
+  * Return: pointer to the string
+  */
+char *cap_string(char *s)
+{
+	int cap;
+	int len;
 
-char *cap_string(char *a)
-{
-int i;
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		if (len == 0)
+		{
+			if (s[len] >= 97 && s[len] <= 122)
+				s[len] -= 32;
+		}
+		else if (s[len] == 9 || s[len] == 10 || s[len] == 32 || s[len] == 32
+				|| s[len] == 34 || s[len] == 40 || s[len] == 41
+				|| s[len] == 44 || s[len] == 46 || s[len] == 59
+				|| s[len] == 63 || s[len] == 123 || s[len] == 125)
+			cap = 1;
+		else if (s[len] >= 97 && s[len] <= 122)
+		{
+			if (cap)
+			{
+				s[len] -= 32;
+				cap = 0;
+			}
+		}
+		else if (s[len] >= 65 && s[len] <= 90)
+			cap = 0;
+		else
+			cap = 0;
 
-if (a[0] >= 'a' && a[0] <= 'z')
-{
-a[0] = a[0] - 32;
-}
-for (i = 1; a[i] != '\0'; i++)
-{
-if ((a[i - 1] == ' ' || a[i - 1] == '\n' || a[i - 1] == '\t'
-|| a[i - 1] == ',' || a[i - 1] == ';' || a[i - 1] == '!'
-|| a[i - 1] == '?' || a[i - 1] == '"' || a[i - 1] == '('
-|| a[i - 1] == ')' || a[i - 1] == '{' || a[i - 1] == '}'
-|| a[i - 1] == '.') && (a[i] > 'a' && a[i] < 'z'))
-{
-a[i] = a[i] - 32;
-}
-}
-return (a);
-}
+	}
+	return (s);
 }
