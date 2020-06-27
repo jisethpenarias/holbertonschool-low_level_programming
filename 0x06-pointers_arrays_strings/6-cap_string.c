ea9dc72a3c0468a6fa618e0850f89d8c57bc1c3e
jpenariasholberton <1875@holbertonschool.com>
2020-06-27 04:01:14 +0000

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,23 @@
 
 char *cap_string(char *a)
 {
-	int i = 0;
+int i;
 
-	char exept[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
-			  '"', '(', ')', '{', '}'};
-
-	while (a[i] != '\0')
-	{
-		int x;
-		int isAnException;
-
-		x = 0;
-		isAnException = 0;
-
-		while (exept[x] != '\0')
-		{
-			if ((i == 0 && exept[x] == a[i]) ||
-			    (i > 0 && exept[x] == a[i - 1]))
-			{
-				isAnException = 1;
-					}
-			x++;
-		}
-
-		if (((a[i] >= 'a') && (a[i] <= 'z')) && isAnException)
-		{
-			a[i] = a[i] - 32;
-		}
-		i++;
-	}
-	return (a);
+if (a[0] >= 'a' && a[0] <= 'z')
+{
+a[0] = a[0] - 32;
+}
+for (i = 1; a[i] != '\0'; i++)
+{
+if ((a[i - 1] == ' ' || a[i - 1] == '\n' || a[i - 1] == '\t'
+|| a[i - 1] == ',' || a[i - 1] == ';' || a[i - 1] == '!'
+|| a[i - 1] == '?' || a[i - 1] == '"' || a[i - 1] == '('
+|| a[i - 1] == ')' || a[i - 1] == '{' || a[i - 1] == '}'
+|| a[i - 1] == '.') && (a[i] > 'a' && a[i] < 'z'))
+{
+a[i] = a[i] - 32;
+}
+}
+return (a);
+}
 }
