e1fe1edb7906284ac8178336e5d041805b425837
jpenariasholberton <1875@holbertonschool.com>
2020-06-28 23:50:57 +0000

    strchr locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in string
+ *
+ *
+ * @s: string to check
+ * @c: char to check for
+ * Return: returns first occurence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ptr = s;
+	int i = 0;
+
+	while (*s != c)
+	{
+		s++;
+		i++;
+	}
+	if (*s == c)
+	{
+		return (ptr + i);
+	}
+	else
+	{
+		return (0);
+	}
+}
