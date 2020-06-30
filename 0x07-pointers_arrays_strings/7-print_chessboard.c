e735b95bdb800d1533b3b84511af08ad0e5f6377
Jiseth <jiseth pena arias>
2020-06-30 17:57:29 -0500

    fix on chesssboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,12 +11,12 @@ int i, j;
 
 i = 0;
 
-  for (i = 0; i < 8; i++)
-  {
-    for (j = 0; j < 8; j++)
-    {
-      _putchar(a[i][j]);
-    }
-    _putchar('\n');
-  }
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
