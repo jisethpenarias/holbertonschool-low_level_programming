6d9fb3e5d6148d0a4e7a3e8e9903db2babb16c53
Jiseth <jiseth pena arias>
2020-06-29 22:01:11 -0500

    strstr localice una subcadena

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - check the code for Holberton School students.
+ * @haystack: esto es toda la cadena
+ * @needle: esto es la comparaicion
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+int x, b, z;
+
+
+for (x = 0; haystack[x] != '\0'; x++)
+{
+for (b = x, z = 0; needle[z] != '\0'; z++, b++)
+{
+if (needle[z] != haystack[b] || haystack == '\0')
+{
+break;
+}
+}
+if (needle[z] == '\0')
+{
+return (haystack + x);
+}
+
+}
+
+return (0);
+}
+
+
