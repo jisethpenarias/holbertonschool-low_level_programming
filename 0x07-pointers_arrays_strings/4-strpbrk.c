d83bc3dcaba2594d3d9eaffb21e88608dce2c551
Jiseth <jiseth pena arias>
2020-06-29 19:13:09 -0500

    strpbrk searches a string for any of a set of byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: string to count in.
+ * @accept: chars to accept.
+ * Return: return pointer.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+int j;
+
+char its;
+char itace;
+
+its = *s;
+itace = *accept;
+
+while (its)
+{
+its = *(s + i);
+j = 0;
+itace = *(accept + j);
+while (itace)
+{
+itace = *(accept + j);
+if (its == itace)
+{
+return (s + i);
+}
+j++;
+}
+i++;
+}
+return (NULL);
+}
