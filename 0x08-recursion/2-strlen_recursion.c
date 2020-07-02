8dd894935849030193513df1f2db280d4a3e727e
Jiseth <jiseth pena arias>
2020-07-01 21:14:02 -0500

    returns the length of a string.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+
+/**
+ *_strlen_recursion - check the code for Holberton School students.
+ * @s: string.
+ * Return: 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+
+if (*s == '\0')
+{
+return (0);
+}
+return (0);
+}
