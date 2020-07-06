1299239d000ea4caf299dbc456dbb5995ec01438
Jiseth Peña <1875@holbertonschool.com>
2020-07-05 23:13:11 -0500

    adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ *@argc: count of the arguments supplied to the program.
+*@argv: array of pointers to the strings wich are those arguments.
+*Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int result;
+	int count;
+
+	result = 0;
+
+	if (argc == 1)
+	{
+		printf("%i\n", 0);
+		return (0);
+	}
+
+	for (count = 1; count < argc; count++)
+	{
+		if (atoi(argv[count]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[count]);
+	}
+
+	printf("%i\n", result);
+
+	return (0);
+}
