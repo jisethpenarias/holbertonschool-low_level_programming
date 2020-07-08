d784496ba984362131ef617ad1961126d1fd9d28
Jiseth <jiseth pena arias>
2020-07-07 21:09:05 -0500

    validations added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,16 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int s1size;
-	int s2size;
-	int i;
-	int j;
+	int s1size, s2size, i, j;
 	char *result;
 
 	s1size = 0;
 	s2size = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[s1size] != 0)
 		s1size++;
@@ -35,9 +37,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != 0; i++)
 		result[i] = s1[i];
 
-	if (s2 != 0 && s1 != 0)
-		result[i] = 32;
-	else if (s1 != 0 && s2 == 0)
+	if (s1 != 0 && s2 == 0)
 	{
 		result[i] = s1[i];
 		return (result);
