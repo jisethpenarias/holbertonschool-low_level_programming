0245acbdc6e7a1e1d57ccc4ae5d1a7f7c445acda
Jiseth <jiseth pena arias>
2020-07-07 21:16:36 -0500

    fix on counter

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,18 +20,16 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[s1size] != 0)
-		s1size++;
-	if (s1size > 0)
+	while (s1[s1size] != '\0')
 		s1size++;
 
-	while (s2[s2size] != 0)
+	while (s2[s2size] != '\0')
 		s2size++;
-	if (s2size > 0)
-		s2size++;
+	
+	s2size++;
 
 	result = malloc((s1size + s2size) * (sizeof(char)));
-	if (result == 0)
+	if (result == '\0')
 		return (NULL);
 
 	for (i = 0; s1[i] != 0; i++)
