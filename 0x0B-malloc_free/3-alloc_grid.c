87ee50a436686683ea18c52f42c47e1c3882ee06
Jiseth <jiseth pena arias>
2020-07-07 23:47:49 -0500

    free array when malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,12 @@ int **alloc_grid(int width, int height)
 	{
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(a[i]);
+			free(a);
 			return (NULL);
+		}
 	}
 
 	return (a);
