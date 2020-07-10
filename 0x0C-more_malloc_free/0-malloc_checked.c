301132a0353ac060a7cc39e362e1d6306f40875f
Jiseth <jiseth pena arias>
2020-07-09 19:43:04 -0500

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory.
+ * @b: size.
+ * Return: pointer.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
