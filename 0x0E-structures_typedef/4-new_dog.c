425a57625f98452988c21795bd413bfa7f34108e
Jiseth <jiseth pena arias>
2020-07-14 20:49:08 -0500

    freeing space

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,14 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * new_dog - creates a new dog
- * @name: name
- * @age: age
- * @owner: owner
- * Return: void.
- */
+* new_dog - creates a new dog
+* @name: name
+* @age: age
+* @owner: owner
+* Return: void.
+*/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -20,13 +19,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	conchi_pointer->name = strdup(name);
-	if (conchi_pointer->name == NULL)
-		return (NULL);
-
 	conchi_pointer->age = age;
 	conchi_pointer->owner = strdup(owner);
-	if (conchi_pointer->owner == NULL)
+
+	if (conchi_pointer->owner == NULL || conchi_pointer->name == NULL)
+	{
+		free(conchi_pointer->owner);
+		free(conchi_pointer->name);
+		free(conchi_pointer);
 		return (NULL);
+	}
 
 	return (conchi_pointer);
 }
