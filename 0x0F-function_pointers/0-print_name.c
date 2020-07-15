f177f1156b07ebf9338d1ae79ae9516f21fbaba7
Jiseth <jiseth pena arias>
2020-07-15 15:39:06 -0500

    description method added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
 
-/*
-*print_name: prints a name
+/**
+*print_name - prints a name
 *@name: pointer char
 *@f: pointer pointing to a function.
+*Return: void.
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == 0 || f == 0)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
