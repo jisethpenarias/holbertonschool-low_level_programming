20705c4781e267c9fec5f05ea9695009e8af3f57
Jiseth <jiseth pena arias>
2020-07-14 21:17:26 -0500

    manually

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,26 +9,35 @@
 * @owner: owner
 * Return: void.
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
+	int j = 0;
 	dog_t *conchi_pointer;
 
 	conchi_pointer = malloc(sizeof(dog_t));
 	if (conchi_pointer == NULL)
 		return (NULL);
-
-	conchi_pointer->name = strdup(name);
-	conchi_pointer->age = age;
-	conchi_pointer->owner = strdup(owner);
-
-	if (conchi_pointer->owner == NULL || conchi_pointer->name == NULL)
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	conchi_pointer->name = malloc((i + 1) * sizeof(char));
+	conchi_pointer->owner = malloc((j + 1) * sizeof(char));
+	if (conchi_pointer->name == NULL || conchi_pointer->owner == NULL)
 	{
-		free(conchi_pointer->owner);
 		free(conchi_pointer->name);
+		free(conchi_pointer->owner);
 		free(conchi_pointer);
 		return (NULL);
 	}
-
+	for (i = 0; name[i] != '\0'; i++)
+		conchi_pointer->name[i] = name[i];
+	conchi_pointer->name[i] = name[i];
+	j = 0;
+	for (j = 0; owner[j] != '\0'; j++)
+		conchi_pointer->owner[j] = owner[j];
+	conchi_pointer->owner[j] = owner[j];
+	conchi_pointer->age = age;
 	return (conchi_pointer);
 }
