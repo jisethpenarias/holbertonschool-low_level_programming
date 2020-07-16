dcdc72e8608c5cf3948378e267d0e72d49cb34c9
Jiseth <jiseth pena arias>
2020-07-15 22:19:32 -0500

    change on argv usage

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 /**
  * main - check the code for Holberton School students.
  * @argc: number of argumenst passed to the program
@@ -16,16 +15,16 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (strcmp(argv[2], "+") != 0 &&
-		strcmp(argv[2], "-") != 0 &&
-		strcmp(argv[2], "*") != 0 &&
-		strcmp(argv[2], "/") != 0 &&
-		strcmp(argv[2], "%") != 0)
+	if (*argv[2] != '+'  &&
+		*argv[2] != '-'  &&
+		*argv[2] != '*'  &&
+		*argv[2] != '/'  &&
+		*argv[2] != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) &&
+	if ((*argv[2] == '/'  || *argv[2] == '%') &&
 		atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
