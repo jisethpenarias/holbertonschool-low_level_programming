040a606cd4b5c5e38ee0ce6c8336a1e451393584
Jiseth <jiseth pena arias>
2020-07-15 21:54:15 -0500

    calc function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of argumenst passed to the program
+ * @argv: array of parameters
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strcmp(argv[2], "+") != 0 &&
+		strcmp(argv[2], "-") != 0 &&
+		strcmp(argv[2], "*") != 0 &&
+		strcmp(argv[2], "/") != 0 &&
+		strcmp(argv[2], "%") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) &&
+		atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char *operator = argv[2];
+
+	int(*operation)(int,int);
+	operation = &(*get_op_func(operator));
+
+	printf("%i",(*operation)(num1,num2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the function adds two integers
+ * @a: int argument.
+ * @b: int argument.
+ * Return: operation result add.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - the function sub two integers
+ * @a: int argument.
+ * @b: int argument.
+ * Return: operation result sub.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - the function adds two integers
+ * @a: int argument.
+ * @b: int argument.
+ * Return: operation result mul.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - the function adds two integers
+ * @a: int argument.
+ * @b: int argument.
+ * Return: operation result div.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - the function modulates two integers
+ * @a: int argument.
+ * @b: int argument.
+ * Return: operation result mod.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
