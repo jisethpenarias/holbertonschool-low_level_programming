c0ca4bb7b204f881d741ba717fcec442e63065c0
Jiseth <jiseth pena arias>
2020-07-18 13:06:36 -0500

    nil fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		aux = va_arg(valist, char *);
-		printf("%s", aux == NULL ? "nil" : aux);
+		printf("%s", aux == NULL ? "(nil)" : aux);
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
