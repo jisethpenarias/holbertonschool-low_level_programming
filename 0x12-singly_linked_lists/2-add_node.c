e6a9e9029ce633a3c0ceab58ce908d4c5f8b0776
jpenariasholberton <1875@holbertonschool.com>
2020-07-26 23:16:38 -0500

    free memory, string looped

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,19 +12,31 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
+	int lenStr;
+	char *copyStr;
+
+	copyStr = strdup(str);
 	/*space for the new node*/
 	newNode = (list_t *)malloc(sizeof(list_t));
 	if (newNode == NULL)
+	{
+		/*in cese of failure free the memory of the newNode*/
+		free(newNode);
+		/*in cese of failure free the memory of the copyStr*/
+		free(copyStr);
 		return (NULL);
+	}
 
 	/*with strdup I double the string it receives as an argument*/
-	newNode->str = strdup(str);
-	newNode->len = strlen(str);
+	newNode->str = copyStr;
+	for (lenStr = 0; copyStr[lenStr];lenStr++)
+	{	
+	}
+	newNode->len = lenStr;
 	/*pointer to the next value in the linked list*/
 	/*next takes the value of the pointer to the next*/
 	/*structure in the likend list*/
 	newNode->next = *head;
-
 	/*change the pointer head whit the new real head on the linked list*/
 	*head = newNode;
 
