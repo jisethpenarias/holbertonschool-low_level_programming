d6cf6adfabab3f3c6100dea1ec19ba629159edc3
jpenariasholberton <1875@holbertonschool.com>
2020-07-28 15:43:28 -0500

    adding conditions

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,10 +16,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	count = 0;
-	while (count != index)
+	while (count != index || head != NULL)
 	{
 		head = head->next;
 		count++;
 	}
+	if (count < index)
+		return (NULL);
 	return (head);
 }
