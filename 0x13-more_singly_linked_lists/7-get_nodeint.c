191d1c36368f02a17699ae48797383ac90f9557d
jpenariasholberton <1875@holbertonschool.com>
2020-07-28 15:35:34 -0500

    change betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,21 +4,22 @@
 /**
  *get_nodeint_at_index - returns the nth node of a linked list.
  *@head: head address.
+ *@index: index of the node.
  * Return: the node´s head ntn.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int count;
+	unsigned int count;
 
-    if (head == NULL)
-        return NULL;
+	if (head == NULL)
+		return (NULL);
 
-    count = 0;
-    while (count != index)
-    {
-       head = head->next;
-       count++;
-    }
-    return (head);
+	count = 0;
+	while (count != index)
+	{
+		head = head->next;
+		count++;
+	}
+	return (head);
 }
