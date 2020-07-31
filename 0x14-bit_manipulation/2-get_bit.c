e0a34a7d3255c23243709b90f79143effdce2a6c
jpenariasholberton <1875@holbertonschool.com>
2020-07-30 21:14:22 -0500

    removing the last if

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,18 +9,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int aux;
-	unsigned int i = 0;
-
-	aux = n;
-	while (aux > 0)
-	{
-		aux = aux >> 1;
-		i++;
-	}
-	i--;
-		if (index > i)
-			return (-1);
-		n = n >> index;
-		return (n % 2);
+	if (index > 63)
+		return (-1);
+	n = n >> index;
+	return (n % 2);
 }
