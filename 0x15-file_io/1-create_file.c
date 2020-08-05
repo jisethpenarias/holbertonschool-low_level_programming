68c320b0d748609e48f0b96fe987bb496382ca92
jpenariasholberton <1875@holbertonschool.com>
2020-08-04 19:20:07 -0500

    adjusting betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
-		wr = write(fd, text_content, i);	
+		wr = write(fd, text_content, i);
 		if (wr == -1)
 			return (-1);
 	}
