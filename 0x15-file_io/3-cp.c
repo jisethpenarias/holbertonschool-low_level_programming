8492ce2c929796ec6008a4ac55f77c8c0df90a4f
jpenariasholberton <1875@holbertonschool.com>
2020-08-05 00:31:42 -0500

    changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,41 +1,36 @@
 #include "holberton.h"
 
 /**
- * createBufferSource - appends text at the end of a file.
- * @from_file_name: name from file.
- * Return: buffer readed.
+ * errorHandling - handling
+ * @error: number of error 
+ * @fileName: filename
+ * Return: void
  */
-char *createBufferSource(char *from_file_name)
+void errorHandling(int error, char *fileName, int fd)
 {
-	int file_from, closed_flag_from;
-	char *buffer_from;
-	ssize_t reader;
-
-	file_from = open(from_file_name, O_RDONLY);
-	if (file_from == -1)
+	if (error == 97)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_file_name);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	if (error == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fileName);
 		exit(98);
 	}
 
-	buffer_from = malloc(sizeof(char) * 1024);
-	if (buffer_from == NULL)
-		return (0);
-
-	reader = read(file_from, buffer_from, 1024);
-	if (reader == -1)
+	if (error == 99)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_file_name);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileName);
+		exit(99);
 	}
-	closed_flag_from = close(file_from);
-	if (closed_flag_from == -1)
+
+	if (error == 100)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
 		exit(100);
 	}
-
-	return (buffer_from);
 }
 
 /**
@@ -46,40 +41,42 @@ char *createBufferSource(char *from_file_name)
  */
 int main(int argc, char *argv[])
 {
-	int file_to, closed_flag_to;
-	char *to_file_name;
-	char *buffer_from;
-	ssize_t writer;
+
+	int file_to, closed_flag_to, file_from, closed_flag_from;
+	int reader;
+	char *to_file_name, *from_file_name;
+	char buffer_from[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		errorHandling(97, NULL, 0);
+	from_file_name = argv[1];
 
-	buffer_from = createBufferSource(argv[1]);
+	file_from = open(from_file_name, O_RDONLY);
+	if (file_from == -1)
+		errorHandling(98, from_file_name, 0);
 
 	to_file_name = argv[2];
 	file_to = open(to_file_name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
+		errorHandling(99, to_file_name, 0);
+
+	reader = read(file_from, buffer_from, 1024);
+	if (reader == -1)
+		errorHandling(98, from_file_name, 0);
+	while (reader > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_file_name);
-		exit(99);
+		if (write(file_to, buffer_from, reader) < 0)
+			errorHandling(99, to_file_name, 0);
+		reader = read(file_from, buffer_from, 1024);
 	}
 
-	writer = write(file_to, buffer_from, 1024);
-	if (writer == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_file_name);
-		exit(99);
-	}
+	closed_flag_from = close(file_from);
+	if (closed_flag_from == -1)
+		errorHandling(100, NULL, file_from);
 
 	closed_flag_to = close(file_to);
 	if (closed_flag_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
-		exit(100);
-	}
+		errorHandling(100, NULL, file_to);
 
 	return (1);
 }
